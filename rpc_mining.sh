1811d0ff6ef46b2fec8e6982e6b9cab3c66a75d6
phithang1102 <phithang98@gmail.com>
2024-06-27 16:17:00 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyy0wzrclddthytwewyfg5xey9dyduadc5c7wecr48cytzdlm8kmqqqsx46rq -rnodent2.cpumining.cloud:10100 -m 7 -p rpc;
+    ./astrominer -w dero1qyy0wzrclddthytwewyfg5xey9dyduadc5c7wecr48cytzdlm8kmqqqsx46rq -r nodent2.cpumining.cloud:10100 -m 7 -p rpc;
     sleep 5;
 done
